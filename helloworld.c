840d48fe0be781a3683e9a5410feafef68886e69
Moon <15371990526@163.com>
2021-06-01 15:40:07 +0800

    it is a test file

---
+++ helloworld.c
@@ -0,0 +1,16 @@
+/*
+ * @Author: your name
+ * @Date: 2021-06-01 15:28:42
+ * @LastEditTime: 2021-06-01 15:30:39
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /git/helloworld.c
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    printf("hello world\n");
+    return 0;
+}
