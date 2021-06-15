47bdd8e0c0774d90deb4a89369fdfbee05b64876
Moon <15371990526@163.com>
2021-06-15 16:37:06 +0800

    试验

--- helloworld.c
+++ helloworld.c
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-06-01 15:28:42
- * @LastEditTime: 2021-06-15 14:57:50
+ * @LastEditTime: 2021-06-15 16:36:18
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /git/helloworld.c
@@ -15,5 +15,6 @@ int main()
     printf("Test\n");
     printf("ok\n");
     printf("vscode\n");
+    printf("nonon\");
     return 0;
 }
