af2b5f7e94a217689a0bb1515f3b913715a75ffa
Moon <15371990526@163.com>
2021-06-15 14:59:17 +0800

    git add helloworld.c

--- helloworld.c
+++ helloworld.c
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-06-01 15:28:42
- * @LastEditTime: 2021-06-01 16:24:38
+ * @LastEditTime: 2021-06-15 14:57:50
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /git/helloworld.c
@@ -14,5 +14,6 @@ int main()
     printf("hello world\n");
     printf("Test\n");
     printf("ok\n");
+    printf("vscode\n");
     return 0;
 }
