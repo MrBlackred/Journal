f723af1b4b5dff4f343c3c232496507ae3c4dcaf
Moon <15371990526@163.com>
2021-06-18 09:23:31 +0800

    第三天测试

--- helloworld.c
+++ helloworld.c
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-06-01 15:28:42
- * @LastEditTime: 2021-06-17 13:42:07
+ * @LastEditTime: 2021-06-18 09:23:16
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /git/helloworld.c
@@ -18,5 +18,6 @@ int main()
     printf("nonon\n");
     printf("2021-6-16\n");
     printf("2021-6-17\n");
+    printf("2021-6-18\n");
     return 0;
 }
