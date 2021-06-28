7ec259111282266cb4071eb29e43622f4d753aec
Moon <15371990526@163.com>
2021-06-28 15:38:26 +0800

    巩固学习

--- helloworld.c
+++ helloworld.c
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-06-01 15:28:42
- * @LastEditTime: 2021-06-18 09:23:16
+ * @LastEditTime: 2021-06-28 15:37:48
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /git/helloworld.c
@@ -19,5 +19,6 @@ int main()
     printf("2021-6-16\n");
     printf("2021-6-17\n");
     printf("2021-6-18\n");
+    printf("2021-6-28\n");
     return 0;
 }
